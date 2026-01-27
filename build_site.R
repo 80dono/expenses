8a87b2787e23cc49426702c50341728beb58d444
80dono <209713000+80dono@users.noreply.github.com>
2026-01-27 14:23:00 -0500

    Resolve duplicate site_libs

--- build_site.R
+++ build_site.R
@@ -1,2 +1,6 @@
+# Clean up directory before rendering site
 setwd(Sys.getenv("HOME_DIRECTORY"))
+unlink(c("_site", "site_libs", "expenses_files"), recursive = TRUE, force = TRUE)
+
 rmarkdown::render_site()
+# unlink(c("site_libs", "expenses_files"), recursive = TRUE, force = TRUE)
