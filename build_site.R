2d91c10e7a8be3e8fafbe4ff0230a44552755b7a
80dono <209713000+80dono@users.noreply.github.com>
2026-01-27 15:59:26 -0500

    Add tithing

--- build_site.R
+++ build_site.R
@@ -3,4 +3,3 @@ setwd(Sys.getenv("HOME_DIRECTORY"))
 unlink(c("_site", "site_libs", "expenses_files"), recursive = TRUE, force = TRUE)
 
 rmarkdown::render_site()
-# unlink(c("site_libs", "expenses_files"), recursive = TRUE, force = TRUE)
